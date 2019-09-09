f7576c8e5bb251576634c371d74acaf4e82100bb
Sasha <Sasha-gezha1@mail.ru>
2019-09-10 01:47:40 +0300

    v1.5.0

---
+++ js/theme.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+if (!fs.existsSync("database.json")) fs.writeFileSync("./database.json", '{"music":[], "settings": [{"theme": "dark"}]}');
+if (!fs.existsSync(`./cache`)) fs.mkdirSync(`./cache`);
+const { remote, ipcRenderer: ipc } = require('electron'),
+	lowdb = require('lowdb'),
+	FileSync = require('lowdb/adapters/FileSync'),
+	adapter = new FileSync("database.json"),
+	db = lowdb(adapter),
+	NodeID3 = require('node-id3');
+
+document.getElementById("theme").href = `css/${db.get("settings").value()[0].theme}.css`;
