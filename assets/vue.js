1cc613bca569ec100830ab3e04fa877734f7f102
Sasha <Sasha-gezha1@mail.ru>
2019-09-28 15:46:54 +0300

    :tutu:

---
+++ assets/vue.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+  el: '#yomp',
+  data: {
+    playlist: [],
+    loved: [],
+    youtuberesult: [],
+    status: {
+      title: "",
+      progress: "",
+      id: 0
+    }
+  },
+  methods: {
+    search() {
+      axios.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + document.getElementById("search").value + "&maxResults=50&key=AIzaSyBBFxx0yqaUfX8V17A4M8UcAiOx-eKXYcs")
+        .then(res => {
+        if (document.getElementById('pl').classList.length == 1) document.getElementById('pl').classList.add("hide");
+        this.youtuberesult = [];
+        res.data.items.forEach(v => {
+          if (v.id.kind == "youtube#video") {
+            this.youtuberesult.push(v);
+          }
+        })
+      })
+    }
+  }
+})
