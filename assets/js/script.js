13d7d7ceb1781424e4b0438503ddce669f55cca8
Sasha <Sasha-gezha1@mail.ru>
2019-09-29 09:49:15 +0300

    reverse

--- assets/js/script.js
+++ assets/js/script.js
@@ -417,7 +417,7 @@ function start() {
 
     function renderPL() {
       var html = [];
-      playList.forEach(function (item, i) {
+      playList.reverse().forEach(function (item, i) {
         item.fav = `<i onclick="love(${item.id}, this);" class="fas fa-heart owo"></i>`;
         if (item.loved == true) item.fav = `<i onclick="love(${item.id}, this);" class="fas fa-heart owo fav"></i>`;
         item.type = '<svg fill="#fff" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">' + '<path d="M0 0h24v24H0z" fill="none"/>' + '<path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>' + '</svg>';
